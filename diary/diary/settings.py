70e9549b99dccb3a7660079b4424433c7af53c6f
prokann <annaaudia5@gmail.com>
2023-05-22 14:32:51 +0300

    Remove debug=true

--- diary/diary/settings.py
+++ diary/diary/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # do it FALSE!
+DEBUG = False # do it FALSE!
 
-ALLOWED_HOSTS = [] # '*'
+ALLOWED_HOSTS = ['*'] # '*'
 #  python manage.py runserver --insecure
 
 
